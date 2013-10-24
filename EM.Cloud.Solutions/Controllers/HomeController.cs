3a2b34ecbbb9564651d127e45d90a0ef65214cb7
Eugene-Murray <eugene.murray@gmail.com>
2013-10-24 20:45:35 +0100

    Add solution and files

---
+++ EM.Cloud.Solutions/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace EM.Cloud.Solutions.Controllers {
+  public class HomeController : Controller {
+    public ActionResult Index() {
+      return View();
+    }
+  }
+}
